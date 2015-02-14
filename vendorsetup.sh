144fff2bd72277f99d3e4c2c829f20eac70828e1
Frank77GLD <pfmzuidam@msn.com>
2015-02-14 15:46:02 +0100

    Update vendorsetup.sh
    add_lunch_combo cm_K00F-userdebug

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
-add_lunch_combo full_K00F-eng
+add_lunch_combo cm_K00F-userdebug
+#add_lunch_combo full_K00F-eng
 #add_lunch_combo full_K00F-userdebug
 #add_lunch_combo full_K00F-evm-userdebug
 #add_lunch_combo cm_K00F-userdebug
